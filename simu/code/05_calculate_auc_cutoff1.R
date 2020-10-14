2ed6b3b97449e1c4d2078191ff3dbd7567fac3c7
Wenpin Hou <wp.hou3@gmail.com>
2020-10-14 07:34:42 -0400

    add changes

---
+++ simu/code/05_calculate_auc_cutoff1.R
@@ -0,0 +1,28 @@
+rm(list=ls())
+source('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/function/01_function.R')
+ddir1 <- '/home-4/whou10@jhu.edu/work-zfs/whou10/metpred/simu/res/'
+ddir2 <- '/home-4/whou10@jhu.edu/work-zfs/whou10/metpred/simu/data/data/'
+rdir1 <- '/home-4/whou10@jhu.edu/work-zfs/whou10/metpred/simu/perf/sensfdr_cutoff1/'
+rdir2 <- '/home-4/whou10@jhu.edu/work-zfs/whou10/metpred/simu/perf/perf_cutoff1/'
+
+af <- list.files(ddir1)
+# af <- af[!af %in% c('4_200_0.3_0.5')]
+library(parallel)
+tmp <- lapply(af, function(f){
+  if (!file.exists(paste0(rdir2, f, '.rds'))){
+    print(f)
+    dir.create(paste0(rdir1, f), recursive = TRUE)
+    am <- list.files(paste0(ddir1, f))
+    perf <- t(sapply(am, function(m){
+      res <- readRDS(paste0(ddir1, f, '/', m))
+      sensfdr <- SensFdr(grep('add:',rownames(res),value=T), res)
+      saveRDS(sensfdr, paste0(rdir1, f, '/', m, '.rds'))
+      AreaUnderSensFdr(sensfdr, cutoff=1)
+    }))
+    rownames(perf) <- sub('_saver.*', '', rownames(perf))
+    saveRDS(perf, paste0(rdir2, f, '.rds'))
+  }  
+  return(0)
+})
+
+
