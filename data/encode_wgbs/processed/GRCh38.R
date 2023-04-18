eaadbec442c8ac8fb035e4f82b6121464e63d732
whou10 <whou10@login01.rockfish.cluster>
2023-04-18 10:20:16 -0400

    add GRCh38.R

--- data/encode_wgbs/processed/GRCh38.R
+++ data/encode_wgbs/processed/GRCh38.R
@@ -1,9 +1,31 @@
-d <- readRDS('/home/whou10/data/whou/metpred/data/encode/wgbs/filter/GRCh38.rds')
-g <- readRDS('/home/whou10/data/whou/metpred/data/encode/rna/proc/GRCh38.rds')
-k <- colMeans(is.na(d))
-d <- d[,k < 0.5]
-g <- g[,colnames(d)]
-k <- rowMeans(is.na(d))
-d <- d[k < 1,]
-saveRDS(d,file='/home/whou10/data/whou/metpred/data/encode/final/GRCh38_wgbs.rds')
-saveRDS(g,file='/home/whou10/data/whou/metpred/data/encode/final/GRCh38_rna.rds')
+d <- readRDS('/home/whou10/data/whou/metpred/data/encode_wgbs/wgbs/proc/GRCh38.rds')
+m <- readRDS('/home/whou10/data/whou/metpred/data/encode_wgbs/meta/combine/wgbs.rds')
+m <- m[match(colnames(d),m$file),]
+k <- sapply(unique(m$Biosample.summary),function(i) {
+  id <- which(m$Biosample.summary==i)
+  if (length(id)==1) {
+    d[,id]
+  } else {
+    rowMeans(d[,id],na.rm=T)
+  }
+})
+saveRDS(k,file='/home/whou10/data/whou/metpred/data/encode_wgbs/processed/GRCh38/wgbs.rds')
+
+
+d <- readRDS('/home/whou10/data/whou/metpred/data/encode_wgbs/rna/proc/GRCh38.rds')
+m <- readRDS('/home/whou10/data/whou/metpred/data/encode_wgbs/meta/combine/rna.rds')
+m <- m[match(colnames(d),m$file),]
+r <- sapply(unique(m$Biosample.summary),function(i) {
+  id <- which(m$Biosample.summary==i)
+  if (length(id)==1) {
+    d[,id]
+  } else {
+    rowMeans(d[,id],na.rm=T)
+  }
+})
+
+r <- r[,colnames(k)]
+
+saveRDS(k,file='/home/whou10/data/whou/metpred/data/encode_wgbs/processed/GRCh38/wgbs.rds')
+saveRDS(r,file='/home/whou10/data/whou/metpred/data/encode_wgbs/processed/GRCh38/rna.rds')
+
