26ed24eb61e85ded1e3827aef1c3ee1646dbc100
whou10 <whou10@c319.rockfish.cluster>
2023-04-18 10:21:30 -0400

    proc.R

--- data/encode_wgbs/wgbs/code/proc.R
+++ data/encode_wgbs/wgbs/code/proc.R
@@ -1,14 +1,16 @@
 library(data.table)
-meta <- readRDS('/home/whou10/data/whou/metpred/data/encode/meta/combine/wgbs.rds')
+meta <- readRDS('/home/whou10/data/whou/metpred/data/encode_wgbs/meta/combine/wgbs.rds')
 conv <- c('GRCh38'='Homo sapiens','mm10'='Mus musculus')
+aaf <- sub('.bed.gz','',list.files('/home/whou10/data/whou/metpred/data/encode_wgbs/wgbs/bed'))
+mean(aaf %in% meta$file)
 for (org in c('GRCh38','mm10')) {
-  af <- meta[which(meta$Organism==conv[org]),'file']
+  af <- intersect(aaf,meta[which(meta$Organism==conv[org]),'file'])
   cc = rep("NULL", 14)
   cc[c(1,6)] <- 'character'
   cc[c(2,11)] <- 'numeric'
   d <- sapply(af,function(f) {
-		      print(f)
-    d <- fread(paste0('/home/whou10/data/whou/metpred/data/encode/wgbs/bed/',f,'.bed.gz'),data.table=F,colClasses=cc)
+    print(f)
+    d <- fread(paste0('/home/whou10/data/whou/metpred/data/encode_wgbs/wgbs/bed/',f,'.bed.gz'),data.table=F,colClasses=cc)
     v <- d[,4]/100
     names(v) <- paste0(d[,1],':',ifelse(d[,3]=='+',d[,2]+1,d[,2]))
     v
@@ -20,6 +22,7 @@ for (org in c('GRCh38','mm10')) {
   for (n in names(d)) {
     m[names(d[[n]]),n] <- d[[n]]
   }
-  saveRDS(m,file=paste0('/home/whou10/data/whou/metpred/data/encode/wgbs/proc/',org,'.rds'))
+  saveRDS(m,file=paste0('/home/whou10/data/whou/metpred/data/encode_wgbs/wgbs/proc/',org,'.rds'))
 }
+
 
