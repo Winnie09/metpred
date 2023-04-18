14e2d7a5e151e2098e1421a3d60e1418c49b03cc
whou10 <whou10@c319.rockfish.cluster>
2023-04-18 10:21:44 -0400

    downloadbed.R

--- data/encode_wgbs/wgbs/code/downloadbed.R
+++ data/encode_wgbs/wgbs/code/downloadbed.R
@@ -1,7 +1,8 @@
-k <- readRDS('/home/whou10/data/whou/metpred/data/encode/meta/combine/wgbs.rds')
+k <- readRDS('/home/whou10/data/whou/metpred/data/encode_wgbs/meta/combine/wgbs.rds')
 library(data.table)
-d <- fread('/home/whou10/data/whou/metpred/data/encode/meta/wgbs/metadata.tsv',data.table=F)
-d <- d[d[,'File format']=='bed bed9+',]
+d <- fread('/home/whou10/data/whou/metpred/data/encode_wgbs/meta/wgbs/metadata.tsv',data.table=F)
+d <- d[d[,'Audit ERROR']!='extremely low coverage',]
 d <- d[d[,'Experiment accession'] %in% k[,'Accession'],]
 #identical(sort(d[,'Experiment accession']),sort(k[,'Accession']))
-writeLines(paste0('wget ',d[,'File download URL'],' -P /home/whou10/data/whou/metpred/data/encode/wgbs/bed'),'/home/whou10/data/whou/metpred/data/encode/wgbs/code/downloadbed.sh')
+writeLines(paste0('wget ',d[,'File download URL'],' -P /home/whou10/data/whou/metpred/data/encode_wgbs/wgbs/bed'),'/home/whou10/data/whou/metpred/data/encode_wgbs/wgbs/code/downloadbed.sh')
+
