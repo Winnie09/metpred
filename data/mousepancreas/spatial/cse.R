2fd96aa804e75a277280e963cfe33e1463db2432
whou10 <wp.hou3@gmail.com>
2023-05-18 22:44:03 -0400

    cse.R

---
+++ data/mousepancreas/spatial/cse.R
@@ -0,0 +1,24 @@
+library(Matrix)
+library(nnls)
+d <- as.matrix(readRDS('/home/whou10/data/whou/metpred/data/mousepancreas/spatial/normexpr.rds'))
+w <- as.matrix(readRDS('/home/whou10/data/whou/metpred/data/mousepancreas/spatial/weight.rds'))
+
+samp <- sub(':.*','',colnames(d))
+
+m <- sapply(unique(samp),function(s) {
+  id <- which(samp==s)
+  sd <- d[,id]
+  sw <- w[id,]
+  cse <- t(apply(sd,1,function(sdr) nnls(sw,sdr)$x))
+  colnames(cse) <- colnames(sw)
+  cse
+},simplify = F)
+
+for (i in 1:length(m)) {
+  colnames(m[[i]]) <- paste0(names(m)[i],':',colnames(m[[i]]))
+}
+
+m <- do.call(cbind,m)
+
+saveRDS(m,file='/home/whou10/data/whou/metpred/data/mousepancreas/spatial/cse.rds')
+
