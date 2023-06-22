97a89fd6c764de2e4d1889f8e002d8113603b755
whou10 <wp.hou3@gmail.com>
2023-06-22 14:25:18 -0400

    02_merged_to_one_matrix.R

---
+++ data/mousepancreas/spatial/imputed_nnls/code/02_merged_to_one_matrix.R
@@ -0,0 +1,7 @@
+alls <- sub('.rds','',list.files('/home/whou10/data/whou/metpred/data/mousepancreas/spatial/imputed_nnls/res/individual/'))
+d <- lapply(alls, function(s){
+  tmp <- readRDS(paste0('/home/whou10/data/whou/metpred/data/mousepancreas/spatial/imputed_nnls/res/individual/', s, '.rds'))
+})
+d <- do.call(cbind, d)
+saveRDS(d, '/home/whou10/data/whou/metpred/data/mousepancreas/spatial/imputed_nnls/res/all/allsample.rds')
+
