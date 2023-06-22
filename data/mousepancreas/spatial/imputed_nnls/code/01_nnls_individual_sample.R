1bf5f8ab3b1b4caea565278e68c3c2f9e77e3418
whou10 <wp.hou3@gmail.com>
2023-06-22 14:25:12 -0400

    02_merged_to_one_matrix.R

---
+++ data/mousepancreas/spatial/imputed_nnls/code/01_nnls_individual_sample.R
@@ -0,0 +1,18 @@
+library(Matrix)
+library(nnls)
+alls <- sub('.rds','',list.files('/home/whou10/data/whou/metpred/data/mousepancreas/spatial/procimpute/res/'))
+for (s in alls){
+  print(s)
+  d <- readRDS(paste0('/home/whou10/data/whou/metpred/data/mousepancreas/spatial/procimpute/res/', s, '.rds'))
+  w <- as.matrix(readRDS('/home/whou10/data/whou/metpred/data/mousepancreas/spatial/proc/all/weight.rds'))
+  w <- w[colnames(d), ]
+  ## Within each sample,
+  ## for each gene, find the non-negative linear least squares (NNLS)
+  ## that solves min||celltype_weight * x - expr||_2. 
+  ## Here, x is celltype-specific gene expression. 
+  cse <- t(apply(d,1,function(sdr) nnls(w,sdr)$x))
+  colnames(cse) <- colnames(w)
+  colnames(cse) <- paste0(s,':',colnames(cse))
+  saveRDS(cse,file=paste0('/home/whou10/data/whou/metpred/data/mousepancreas/spatial/imputed_nnls/res/', s, '.rds'))
+}
+
