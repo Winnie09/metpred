5878ed86497f4bf566d27c8e92c7a7e189cc30f7
whou10 <wp.hou3@gmail.com>
2023-07-07 09:38:36 -0400

    trainpredict.R

--- software/trainpredict.R
+++ software/trainpredict.R
@@ -18,8 +18,8 @@ trainpredict <- function(trainexpr,testexpr,trainmeth) {
   trainmeth <- log(trainmeth/(1-trainmeth))
   
   int <- intersect(rownames(trainexpr),rownames(testexpr))
-  trainexpr <- trainexpr[int,]
-  testexpr <- testexpr[int,]
+  trainexpr <- trainexpr[int,,drop=FALSE]
+  testexpr <- testexpr[int,,drop=FALSE]
   colnames(trainmeth) <- colnames(trainexpr) <- paste0('train_',1:ncol(trainexpr))
   oritestname <- colnames(testexpr)
   names(oritestname) <- colnames(testexpr) <- paste0('test_',1:ncol(testexpr))
@@ -30,10 +30,10 @@ trainpredict <- function(trainexpr,testexpr,trainmeth) {
   expr <- apply(expr,2,function(i) qnem[frank(i)])
   dimnames(expr) <- dn
   
-  trainexpr <- expr[,paste0('train_',1:ncol(trainexpr))]
-  testexpr <- expr[,paste0('test_',1:ncol(testexpr))]
+  trainexpr <- expr[,paste0('train_',1:ncol(trainexpr)),drop=FALSE]
+  testexpr <- expr[,paste0('test_',1:ncol(testexpr)),drop=FALSE]
   
-  trainexpr <- trainexpr[rowMeans(trainexpr > median(trainexpr[trainexpr > 0])) >= 0.1,]
+  trainexpr <- trainexpr[rowMeans(trainexpr > median(trainexpr[trainexpr > 0])) >= 0.1,,drop=FALSE]
   m <- rowMeans(trainexpr)
   s <- rowsds(trainexpr,m)
   stdtrainexpr <- (trainexpr-m)/s
@@ -78,7 +78,7 @@ trainpredict <- function(trainexpr,testexpr,trainmeth) {
     }
   }
   
-  ### cross validation
+  print('start cross validation...')
   set.seed(12345)
   spid <- split(sample(colnames(trainexpr)),ceiling(seq_along(colnames(trainexpr))/(ncol(trainexpr)/5)))
   perf <- NULL
@@ -91,7 +91,10 @@ trainpredict <- function(trainexpr,testexpr,trainmeth) {
     
     lambdalist <- c(10^c(-1,0,1,2))
     for (clunum in clunumlist) {
+      print(paste0('clunum ', clunum))	    
       pred <- inpredfunc(cluexpr[[as.character(clunum)]][,cvtrainid],cluexpr[[as.character(clunum)]][,cvtestid],cvtrainmeth,lambdalist)
+      print('0...')
+      print(str(pred))
       mse <- sapply(pred,function(i) rowMeans((i-cvtestmeth)^2))
       colnames(mse) <- lambdalist
       mse <- melt(mse)
@@ -101,19 +104,28 @@ trainpredict <- function(trainexpr,testexpr,trainmeth) {
   }
   perf$combpara <- paste0(perf$clunum,'_',perf$lambda)
   perf <- aggregate(perf$mse,list(perf$combpara,perf$cpg),mean)
-  perf <- perf[order(perf$x),]
-  perf <- perf[!duplicated(perf[,2]),]
+  perf <- perf[order(perf$x),,drop=FALSE]
+  print('1...')
+  print(str(perf))
+
+  perf <- perf[!duplicated(perf[,2]),,drop=FALSE]
+  print('2...')
+  print(str(perf)) 
   
+  print('information...')
   print(table(perf[,1]))
+  print(str(trainexpr))
+  print(str(testexpr))
   pred <- sapply(unique(perf[,1]),function(uset) {
     id <- which(perf[,1]==uset)
     clunum=sub('_.*','',uset)
     inpredfunc(cluexpr[[clunum]][,colnames(trainexpr)],cluexpr[[clunum]][,colnames(testexpr)],trainmeth[id,,drop=F],lambda=as.numeric(sub('.*_','',uset)))
   },simplify = F)
   pred <- do.call(rbind,pred)
-  
+  print(str(pred))
   colnames(pred) <- oritestname[colnames(pred)]
-  pred[rownames(trainmeth),]
+  #pred[rownames(trainmeth),]
+  pred
 }
 
 
