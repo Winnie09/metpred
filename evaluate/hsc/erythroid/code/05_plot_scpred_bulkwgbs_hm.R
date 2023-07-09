68e2c7f9fb05b85c76011e9a7e4463c5ea8c6aef
whou10 <wp.hou3@gmail.com>
2023-07-08 20:54:42 -0400

    05_plot_scpred_bulkwgbs_hm.R

--- evaluate/hsc/erythroid/code/05_plot_scpred_bulkwgbs_hm.R
+++ evaluate/hsc/erythroid/code/05_plot_scpred_bulkwgbs_hm.R
@@ -6,7 +6,13 @@ dev.off()
 
 w3 = readRDS('/home/whou10/data/whou/metpred/evaluate/hsc/erythroid/bulk/bulk_wgbs.rds')
 library(pheatmap)
-pdf('/home/whou10/data/whou/metpred/evaluate/hsc/erythroid/plot/bulk_wgbs_hm.pdf')
+png('/home/whou10/data/whou/metpred/evaluate/hsc/erythroid/plot/bulk_wgbs_hm.png')
 pheatmap(w3, cluster_rows = F, cluster_cols = F, show_rownames = F, labels_row = 'top 1e4 CpGs largest mean difference')
+dev.off()
+
+me = readRDS(file='/home/whou10/data/whou/metpred/data/hsc/proc/wgbs_matchedtrain.rds')
+library(pheatmap)
+png('/home/whou10/data/whou/metpred/evaluate/hsc/erythroid/plot/wgbs_training_top1e4cpg.png')
+pheatmap(me[rownames(pred2), ], cluster_rows = F, cluster_cols = F, show_rownames = F, labels_row = 'top 1e4 CpGs largest mean difference')
 dev.off()
 
