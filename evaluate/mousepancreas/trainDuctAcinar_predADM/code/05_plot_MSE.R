64fb8e179716c0d95aa79808ecec440f9a7cfec6
whou10 <wp.hou3@gmail.com>
2023-10-15 22:15:45 -0400

    05_plot_MSE.R

---
+++ evaluate/mousepancreas/trainDuctAcinar_predADM/code/05_plot_MSE.R
@@ -0,0 +1,18 @@
+setwd('/home/whou10/data/whou/metpred/')
+source('/home/whou10/scratch16/whou10/resource/startup.R')
+dir <- 'evaluate/mousepancreas/trainDuctAcinar_predADM/perf/mse/'
+allf <- list.files(dir)
+cc <- lapply(allf, function(f){
+  tmp <- readRDS(paste0(dir, f))
+  chr <- sub(':.*', '', sub('_.*', '', names(tmp)))
+  tmp <- tmp[chr %in% paste0('chr', c(seq(1,22), 'X', 'Y'))]
+  set.seed(12345)
+  tmp <- sample(tmp, min(1e2, length(tmp)), replace = F)
+  tmp2 <- data.frame(cor = tmp,  
+                     sd = paste0('[',sub('.rds', '', sub('testsd_cpg_', '', f)),')'))
+})
+cc2 <- do.call(rbind, cc)
+acrossRowCor_plot(plotdata = cc2,
+                  ylab = 'across-sample MSE',
+                  savefile = TRUE,
+                  filename = 'evaluate/mousepancreas/trainDuctAcinar_predADM/plot/across_sample_mse.pdf')
