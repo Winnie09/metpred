bfccdee532ad5a8c7cac218e363ebe24462e4c24
whou10 <wp.hou3@gmail.com>
2024-09-23 10:52:44 -0400

    02_pred_allcpg.R

--- evaluate/pbmc/predsc/imp/code/pred/02_pred_allcpg.R
+++ evaluate/pbmc/predsc/imp/code/pred/02_pred_allcpg.R
@@ -1,5 +1,4 @@
 cpggroup = as.numeric(commandArgs(trailingOnly = T)[1])
-
 source('/home/whou10/data/whou/metpred/software/trainpredict.R')
 source('/home/whou10/scratch16/whou10/resource/startup.R')
 ddir <- '/home/whou10/data/whou/metpred/data/pbmc/imp/res/'
@@ -21,31 +20,11 @@ str(r.tr)
 # - attr(*, "dimnames")=List of 2
 # ..$ : chr [1:58434] "TSPAN6" "TNMD" "DPM1" "SCYL3" ...
 # ..$ : chr [1:66] "blueprint_venous_blood-C000S5-CD14-positive_CD16-negative_classical_monocyte" "blueprint_venous_blood-C000S5-mature_neutrophil" "blueprint_venous_blood-C0010K-CD14-positive_CD16-negative_classical_monocyte" "blueprint_venous_blood-C0010K-mature_neutrophil" ...
-r.te = readRDS(paste0(ddir, 'rna_test_sc.rds'))
-str(r.te)
-# num [1:8209, 1:37740] 0.0963 0.3414 0.0868 0.0426 0.1903 ...
-# - attr(*, "dimnames")=List of 2
-# ..$ : chr [1:8209] "NOC2L" "ISG15" "TNFRSF18" "TNFRSF4" ...
-# ..$ : chr [1:37740] "b_cells:AAACATACAATGCC-1" "b_cells:AAACATACACGCAT-1" "b_cells:AAACATACGAATAG-1" "b_cells:AAACATACGTGTCA-1" ...
-# selcpg = readRDS('/home/whou10/data/whou/metpred/evaluate/pbmc/imp/res/selcpg/top1e4_mono_t_diff_greater0.2_tr_te.rds')
-ct = sub(':.*', '', colnames(r.te))
-
-
-r.te = r.te[, ct %in% c('b_cells', 'cd14_monocytes', 'cd56_nk', 
-                        'cytotoxic_t', 'memory_t', 'naive_t',
-                        'regulatory_t') ]
-ct = sub(':.*', '', colnames(r.te))
-id <- unlist(sapply(unique(ct), function(uct){
-  which(ct == uct)[1:300]
-}, simplify = F))
-r.te.sub = r.te[,id]
-str(r.te.sub)
-
+r.te.sub = readRDS(paste0(ddir, 'rna_test_sc_sub300.rds'))
+ 
 pred <- trainpredict(trainexpr=r.tr,
                      testexpr=r.te.sub,
                      trainmeth=w.tr,
                      clunumlist = c(1e3),
                      lambdalist = c(10e-1))
 saveRDS(pred, paste0('/home/whou10/data/whou/metpred/evaluate/pbmc/predsc/imp/res/pred/allcpg/pred_cpggroup', cpggroup, '.rds'))
-
-
