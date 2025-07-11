6e3b90302ce05c433867141a600987da8347b69b
whou10 <wp.hou3@gmail.com>
2025-07-11 14:09:46 -0400

    add download sh 3

---
+++ data/tcga/hg38/2024/wgbs/raw/download/rna/download.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+#SBATCH -A hji7_bigmem
+#SBATCH --partition=bigmem
+#SBATCH --time=23:58:59
+#SBATCH --nodes=1
+#SBATCH --ntasks=22
+#SBATCH -o %x.o%j
+
+
+wget https://portal.gdc.cancer.gov/files/094bd0ca-5f46-4ff7-b674-04c5b9001951
+wget https://portal.gdc.cancer.gov/files/23bf74db-bb4e-44c5-8473-e651b818e460
+wget https://portal.gdc.cancer.gov/files/b97a37c0-eda8-44c5-9bd2-606a708bdb75
+wget https://portal.gdc.cancer.gov/files/739b3fc4-0924-4d13-aecc-acc93cb9f486
+wget https://portal.gdc.cancer.gov/files/4756b201-87c2-4428-b2cd-f933422fe8cf
+wget https://portal.gdc.cancer.gov/files/edfdd711-4764-43a7-9948-4320dc17b2b1
+wget https://portal.gdc.cancer.gov/files/94861d8a-3b40-4334-9fb5-c9df71a1e1da
+wget https://portal.gdc.cancer.gov/files/129f7ee5-6342-4a24-9317-d52462ce6a70
+wget https://portal.gdc.cancer.gov/files/4756b201-87c2-4428-b2cd-f933422fe8cf
+wget https://portal.gdc.cancer.gov/files/cd03b494-38bf-4b7d-86a5-672acd7d0125
+wget https://portal.gdc.cancer.gov/files/7b12afae-a674-4da8-8782-fa44a174b103
+wget https://portal.gdc.cancer.gov/files/a651eed5-ebad-4546-97f1-5816ffdea752
+wget https://portal.gdc.cancer.gov/files/57665766-9632-4bd7-bc1c-54adc5c97b3d
